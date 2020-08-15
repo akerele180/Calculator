0de67c7c8f1c9a818a5e0eae1656b7d3429fa012
Akerele, Tosin James <akerele.james1699@gmail.com>
2020-08-15 11:26:25 +0100

    Add files via upload

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,57 @@
-function make(){
-    var x=document.getElementById("display");
+var input=[];
+input.push(5);
+alert(input);
+//------INTRODUCTORY DISPLAY MESSAGE------//
+//var x=document.getElementById("display");
+function answer(){
+    let x=document.getElementById("display");
     x.innerHTML="CALCULATOR";
+}
+//-----------CLEAR SCREEN------------------//
+function clearScreen(){
+    let x=document.getElementById("display");
+    x.innerHTML="";
+}
+function pops(){
+    let x=document.getElementById("display");
+    x.pop();
+}
+//--------------FIGURES DISPLAY------------//
+function one(){
+    let x=document.getElementById("display");
+    x.innerHTML="1";
+}function two(){
+    let x=document.getElementById("display");
+    x.innerHTML="2";
+}function three(){
+    let x=document.getElementById("display");
+    x.innerHTML="3";
+}function four(){
+    let x=document.getElementById("display");
+    x.innerHTML="4";
+}function five(){
+    let x=document.getElementById("display");
+    x.innerHTML="5";
+}function six(){
+    let x=document.getElementById("display");
+    x.innerHTML="6";
+}function seven(){
+    let x=document.getElementById("display");
+    x.innerHTML="7";
+}function eight(){
+    let x=document.getElementById("display");
+    x.innerHTML="8";
+}function nine(){
+    let x=document.getElementById("display");
+    x.innerHTML="9";
+}function doubleZero(){
+    let x=document.getElementById("display");
+    x.innerHTML="00"
+}function dot(){
+    let x=document.getElementById("display");
+    x.innerHTML="."
+}
+//------------TEXT CONCATENATION-----------//
+function concatenate(){
+
 }
